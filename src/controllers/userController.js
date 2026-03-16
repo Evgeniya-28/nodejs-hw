6dc8e173c100d7791547b707ad1fc3333b62cbe7
Evgeniya Kosykh <evgeniyapylypenko92@gmail.com>
2026-03-16 22:48:13 +0200

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,10 @@
-// src/controllers/userController.js
-
-import createHttpError from "http-errors";
-import { User } from "../models/user.js";
-import { saveFileToCloudinary } from "../utils/saveFileToCloudinary.js";
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 const updateUserAvatar = async (req, res) => {
   if (!req.file) {
-    throw createHttpError(400, "No file");
+    throw createHttpError(400, 'No file');
   }
 
   const uploadResult = await saveFileToCloudinary(req.file.buffer);
@@ -14,10 +12,12 @@ const updateUserAvatar = async (req, res) => {
   const user = await User.findByIdAndUpdate(
     req.user._id,
     { avatar: uploadResult.secure_url },
-    { new: true },
+    { returnDocument: 'after' },
   );
 
-  res.status(200).json({ url: user.avatar });
+  res.status(200).json({
+    url: user.avatar,
+  });
 };
 
 export { updateUserAvatar };
