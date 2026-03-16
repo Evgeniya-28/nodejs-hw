75f350384a94ff5dc07af3df0503900cf39b6c56
Evgeniya Kosykh <evgeniyapylypenko92@gmail.com>
2026-03-16 20:30:13 +0200

    add all

---
+++ src/models/note.js
@@ -0,0 +1,41 @@
+//  src\models\note.js
+
+import { model, Schema } from "mongoose";
+import { TAGS } from "../constants/tags.js";
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    tag: {
+      type: String,
+      enum: TAGS,
+      default: "Todo",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+noteSchema.index(
+  { title: "text", content: "text" },
+  {
+    name: "NoteTextIndex",
+    weights: { title: 10, content: 2 },
+    default_language: "english",
+  },
+);
+
+const Note = model("Note", noteSchema);
+
+export { Note };
